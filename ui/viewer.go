5d85ac4c9f54b0fcbe8fd072bea73baf911fcfe5
Bharath <devallabharath619@gmail.com>
2024-12-15 15:56:14 -0500

    feat: ✨ Implemented Tabs

--- ui/viewer.go
+++ ui/viewer.go
@@ -1,14 +1,19 @@
 package ui
 
 import (
-	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/storage"
+	"fyne.io/fyne/v2/widget"
 	"github.com/devallabharath/md-notes/core"
 )
 
 // MainView :: main view of the app
-func MainView() *fyne.Container {
+func MainView() *container.Split {
 	side := NewFileTree(storage.NewFileURI(core.Config.Path))
-	return container.NewHBox(side)
+	tabs := container.NewAppTabs()
+	label := widget.NewLabel("Label")
+	tab := container.NewTabItem("test.md", label)
+	tabs.Append(tab)
+
+	return container.NewHSplit(side, tabs)
 }
